19c9a7f0b6eaf2ba0c59b3ec750b967f2d168ae4
name <name@gmail.com>
2022-02-16 13:38:37 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Git_Test
+{
+    public class Class1
+    {
+        int x;
+        static void Main()
+        {
+            Class1 c = new Class1();
+            
+        }
+    }
+}
